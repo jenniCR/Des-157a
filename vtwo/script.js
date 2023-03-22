323d0adb01575da2f1f89d41c1e64fcd560c0071
jenniCR <jenni.prime.illust@gmail.com>
2023-03-21 22:46:31 -0700

    test results

---
+++ vtwo/script.js
@@ -0,0 +1,106 @@
+(function(){
+
+    "use strict";
+    console.log(`reading js`);
+    
+
+    const overlay = document.getElementById("overlay");
+    const hp1 = document.getElementById("hp1"); //need one for hp2, peck, dodge, start
+    const hp2 = document.getElementById("hp2");
+    const peck = document.getElementById("peck");
+    const dodge = document.getElementById("dodge");
+    const action = document.getElementById("action");
+    const audio = new Audio("audio/BIRD_1.mp3");
+    var audioElement = document.getElementById('bgMusic')
+    let healthBar1 = document.querySelector("#healthBar1 .inner");
+    let healthBar2 = document.querySelector("#healthBar2 .inner");
+
+    audioElement.addEventListener("canplay", function(event){
+        audioElement.volume = ".2";
+    }, false);
+
+    const gameData = {
+        
+        players: ["player one", "player two"],
+        score: [200, 200],
+        roll: 0,
+        index: 0
+    }
+    
+        peck.addEventListener("click",function(){
+
+            gameData.roll = Math.floor(Math.random() * 50) + 1;
+    
+            if (gameData.index == 0){ 
+                hp2.innerHTML = `HP: ${gameData.score[1]}/200`;
+                gameData.score[1] = gameData.score[1] - gameData.roll;
+                action.innerHTML = "<h3>Player One Pecks...</h3>";
+                healthBar2.style.width = `${gameData.score[1]/2}%`;
+                setTimeout(() => {
+                    action.innerHTML = "<h3>What would Player Two do?</h3>";
+                }, 3000);
+                
+            } else {
+                hp1.innerHTML = `HP: ${gameData.score[0]}/200`;
+                gameData.score[0] = gameData.score[0] - gameData.roll;
+                action.innerHTML = "<h3>Player Two Pecks...</h3>";
+                healthBar1.style.width = `${gameData.score[0]/2}%`;
+                setTimeout(() => {
+                    action.innerHTML = "<h3>What would Player One do?</h3>";
+                }, 3000);
+                
+            }
+            checkWinningCondition();
+            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+    
+        });
+    
+    
+        dodge.addEventListener("click",function(){
+            gameData.roll = Math.floor(Math.random() * 50) + 1;
+    
+            if (gameData.index == 0){ 
+                gameData.score[0] = gameData.score[0] + gameData.roll;//add a rule that stops it from going over 200hp and less than zero
+                hp1.innerHTML = `HP: ${gameData.score[0]}/200`;
+                healthBar1.style.width = `${gameData.score[0]/2}%`;
+                action.innerHTML = "<h3>Player One Dodges...</h3>";
+                setTimeout(() => {
+                action.innerHTML = "<h3>What would Player Two do?...</h3>";
+                }, 3000);
+               
+            } else {
+                gameData.score[1] = gameData.score[1] + gameData.roll;
+                hp2.innerHTML = `HP: ${gameData.score[1]}/200`;
+                healthBar2.style.width = `${gameData.score[1]/2}%`;
+                action.innerHTML = "<h3>Player Two Dodges...</h3>";
+                setTimeout(() => {
+                action.innerHTML = "<h3>What would Player One do?</h3>";
+                }, 3000);
+            }
+            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+    
+        });
+        
+    function checkWinningCondition() {
+        let winner = "";
+        
+        if (gameData.index == 0){
+            winner = "Player One";
+        if (gameData.score[1] < 1){
+            action.innerHTML = `<h3>${winner} has won the battle! Hooray!</h3>`; //add a rule that prevents this rule from being
+            document.querySelector(".attack").innerHTML = "";
+            audio.play();
+       }
+    }
+        else if (gameData.index == 1) {
+            winner = "Player Two";
+            if (gameData.score[0] < 1){
+            action.innerHTML = `<h3>${winner} has won the battle! Hooray!</h3>`;
+            document.querySelector(".attack").innerHTML = ""; 
+            audio.play();
+       }
+    }
+            
+    }
+
+})();
